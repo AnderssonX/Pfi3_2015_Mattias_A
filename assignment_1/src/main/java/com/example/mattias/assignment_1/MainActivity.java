cf1f58005b30c74c421fbfdcdfb5b5de2008dc54
Mattias Andersson <mattias8012@gmail.com>
2015-04-01 23:30:35 +0200

    Added awesome stuff!

--- assignment_1/src/main/java/com/example/mattias/assignment_1/MainActivity.java
+++ assignment_1/src/main/java/com/example/mattias/assignment_1/MainActivity.java
@@ -1,26 +1,43 @@
 package com.example.mattias.assignment_1;
 
-import android.support.v7.app.ActionBarActivity;
+//
+// Yoda Says....
+// By Mattias Andersson
+// IDK14, Malmo Hogskola, 2015
+//
+
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 import java.util.Random;
 
 
 public class MainActivity extends ActionBarActivity {
+// various variables for checking previous quote
+    int lastQuote;
+int prevQuote=6;
+    // create a mediaplayer
+MediaPlayer mySound;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mySound = MediaPlayer.create(this, R.raw.lightsaber);
+        mySound.start();
     }
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
@@ -42,12 +59,100 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void getQuote(View view) {
+        if (mySound != null) {
+            if (mySound.isPlaying()) {
+                mySound.stop();
+            }
+            mySound.release();
+            mySound = null;
+        }
+
+        prevQuote=lastQuote;
         TextView quoteView = (TextView)findViewById(R.id.quoteMessage);
         String[] randomQuotes = getResources().getStringArray(R.array.quotes);
         Random random = new Random();
-        int randomNo = random.nextInt(4);
+        int randomNo = random.nextInt(6);
+        lastQuote=randomNo;
+
     quoteView.setText(randomQuotes[randomNo]);
+        // quoteView.animate();
+    quoteView.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, android.R.anim.slide_in_left));
+// check value of random number and play appropriate sound file to fit current quote!
+if(randomNo==0){
+mySound = MediaPlayer.create(this, R.raw.years);
+mySound.start();
+}
+        if(randomNo==1){
+            mySound = MediaPlayer.create(this, R.raw.truly);
+            mySound.start();
+        }
+        if(randomNo==2){
+            mySound = MediaPlayer.create(this, R.raw.darkside);
+            mySound.start();
+        }
+        if(randomNo==3){
+            mySound = MediaPlayer.create(this, R.raw.doordonot);
+            mySound.start();
+        }
+        if(randomNo==4){
+            mySound = MediaPlayer.create(this, R.raw.clouds);
+            mySound.start();
+        }
+        if(randomNo==5){
+            mySound = MediaPlayer.create(this, R.raw.beware);
+            mySound.start();
+        }
+        Log.i("Button pressed", "fetching new random quote");
+Log.isLoggable("Random No is equal to", randomNo);
+    }
+public void lastQuote(View view) {
+    // Check if MediaPlayer is currently playing and stop it if it does.
+    if (mySound != null) {
+        if (mySound.isPlaying()) {
+            mySound.stop();
+        }
+        mySound.release();
+        mySound = null;
+    }
+
+    TextView quoteView = (TextView)findViewById(R.id.quoteMessage);
+            String[] randomQuotes = getResources().getStringArray(R.array.quotes);
+    // If prevQuote is seperated from 5, display it in textview.
+    if(prevQuote!=6){
+    quoteView.setText(randomQuotes [prevQuote]);
+    quoteView.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, android.R.anim.slide_in_left));
+// play approriate sound file to fit previous quote
+
+        if(prevQuote==0){
+            mySound = MediaPlayer.create(this, R.raw.years);
+            mySound.start();
+        }
+        if(prevQuote==1){
+            mySound = MediaPlayer.create(this, R.raw.truly);
+            mySound.start();
+        }
+        if(prevQuote==2){
+            mySound = MediaPlayer.create(this, R.raw.darkside);
+            mySound.start();
+        }
+        if(prevQuote==3){
+            mySound = MediaPlayer.create(this, R.raw.doordonot);
+            mySound.start();
+        }
+        if(prevQuote==4){
+            mySound = MediaPlayer.create(this, R.raw.clouds);
+            mySound.start();
+        }
+        if(prevQuote==5){
+            mySound = MediaPlayer.create(this, R.raw.beware);
+            mySound.start();
+        }
+
+        // logcat stuff
+        Log.i("Button pressed", "grabbing previous displayed quote");}
+    else{
+        Log.i("Button pressed", "But can't find a previous quote");}
+    }
 }
 
-    }
 
